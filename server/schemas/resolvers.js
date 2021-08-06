bd00593ae7e33e61278d24990e3c8882eab61c47
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-08-06 17:18:33 -0600

    server/schemas/resolvers me query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // get a single user
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    }
+};
+
+module.exports = resolvers;
