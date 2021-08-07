8f344400f42c091702b88fb4f077280df4e0a079
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-08-07 11:44:34 -0600

    client/comoponents/LoginForm.js update

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,10 @@
-// UPDATE THIS FILE
-
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -28,15 +28,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -45,7 +41,7 @@ const LoginForm = () => {
     setUserFormData({
       username: '',
       email: '',
-      password: '',
+      password: ''
     });
   };
 
